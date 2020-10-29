cc51c07af1b3882f0769a45d25c8eb8892b3dbc3
Coding With The Force <codingwiththeforce@gmail.com>
2020-10-29 00:07:00 -0500

    Added comments

--- lwc_utility_modules/demo_component/demo_component.js
+++ lwc_utility_modules/demo_component/demo_component.js
@@ -1,4 +1,7 @@
 import { LightningElement } from 'lwc';
+
+//Importing in our javascript utility module. The import variable names (OPEN_STATUS, showError, util_class)
+//are the names of the exported variable, method and class in our util_module component
 import { OPEN_STATUS, showError, util_class} from 'c/util_module';
 
 export default class Demo_component extends LightningElement 
@@ -7,11 +10,19 @@ export default class Demo_component extends LightningElement
     returnedError;
     newStatus;
 
+    //The connectedCallback method runs on component load
     connectedCallback()
     {
+        //assigning our imported OPEN_STATUS value to our local openStatus variable
         this.openStatus = OPEN_STATUS;
+
+        //assigning our imported showError methods return value to our local returnedError variable 
         this.returnedError = showError('Tacos had an error');
+
+        //assigning our imported class's newStatus variable to our local newStatus variable
         this.newStatus = new util_class().newStatus;
+
+        //Calling our imported class's showConsoleLog method
         new util_class().showConsoleLog();
     }
 }
--- lwc_utility_modules/util_module/util_module.js
+++ lwc_utility_modules/util_module/util_module.js
@@ -1,9 +1,19 @@
+
+//DISCLAIMER: There are many different ways available in ES6 JS to export variables, functions and classes
+//you can find out more about exports here: https://developer.mozilla.org/en-US/docs/web/javascript/reference/statements/export
+
+//How to export a variable and make it available when importing your module
+//in other components
 export const OPEN_STATUS = 'Open';
 
+//How to export a method and make it available when importing your module
+//in other components
 export function showError(error) {
     return 'This is an error ' + error;
 }
 
+//How to export a class and make it available when importing your module
+//in other components
 export class util_class {
 
     newStatus = 'New';
